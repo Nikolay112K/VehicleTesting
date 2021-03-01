99f389fb60a17b9d88857fc986c3f589200db263
DESKTOP-QFTHJVS\Nik <kolya-kirich@i.ua>
2021-03-01 13:32:10 +0100

    Add more function

--- VehicleTesting/GroundVehicles/Trum.cs
+++ VehicleTesting/GroundVehicles/Trum.cs
@@ -8,11 +8,29 @@ namespace VehicleTesting.GroundVehicles
 {
     public class Trum : TechnicalVehicle
     {
-        public Wheels Wheels { get; set; }
+        public Odometr OdometrObj { get; set; }
+        public Mirrors MirrorsObj { get; set; }
+        public WaterProof WaterProofObj { get; set; }
+        public Seats SeatsObj { get; set; }
+        public Roof RoofObj { get; set; }
+        public Doors DoorsObj { get; set; }
+        public Wheels WheelsObj { get; set; }
         public string EngineHealth;
-        public int Seats;
         public string BodyHealth;
         public int Lights;
-    }
+        public Trum(string engineHealth, string bodyHealth, int lights)
+        {
+            this.EngineHealth = engineHealth;
+            this.BodyHealth = bodyHealth;
+            this.Lights = lights;
 
+            this.OdometrObj = new Odometr();
+            this.MirrorsObj = new Mirrors();
+            this.WaterProofObj = new WaterProof();
+            this.SeatsObj = new Seats();
+            this.RoofObj = new Roof();
+            this.DoorsObj = new Doors();
+            this.WheelsObj = new Wheels();
+        }
+    }
 }
--- VehicleTesting/SpareParts/Doors.cs
+++ VehicleTesting/SpareParts/Doors.cs
@@ -6,7 +6,20 @@ namespace VehicleTesting.SpareParts
 {
     public class Doors
     {
-        public int CountOfDoors =4;
+        public int CountOfDoors = 4;
+        public int DoorsHealth = 1000;
+        public Doors()
+        {
 
+        }
+        public void DoorIsOpen()
+        {
+            Console.WriteLine("Door is Opend.");
+        }
+        public void DoorIsClosed()
+        {
+            Console.WriteLine("Door is Closed");
+            DoorsHealth--;
+        }
     }
 }
